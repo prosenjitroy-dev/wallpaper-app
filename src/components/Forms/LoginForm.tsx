4ef00cdd7aa81335313d8cf736e06c79c3d8ef0c
Prosenjit Roy <prosenjitroy.dev@gmail.com>
2026-01-21 15:57:06 +0530

    Implement LoginForm UI with validation and controls
    Refactored LoginForm to use react-hook-form's Controller for email and password fields, added UI components from shadcnui, and included validation with zod. The form now displays input fields, error messages, a submit button with loading state, and a checkbox (currently labeled as 'Password'). Updated dependencies in package.json for next, @types/react, and eslint-config-next.

--- src/components/Forms/LoginForm.tsx
+++ src/components/Forms/LoginForm.tsx
@@ -1,11 +1,21 @@
 "use client";
 
-import { loginSchema } from "@/lib/zodSchema";
+import { loginSchema, LoginSchematype } from "@/lib/zodSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+
+import { Controller, useForm } from "react-hook-form";
+import { Field, FieldError, FieldLabel } from "../shadcnui/field";
+import { Input } from "../shadcnui/input";
+import { Checkbox } from "../shadcnui/checkbox";
+import { Button } from "../shadcnui/button";
+import { FingerprintIcon, LoaderIcon } from "lucide-react";
 
 const LoginForm = () => {
-  const {} = useForm({
+  const {
+    handleSubmit,
+    control,
+    formState: { isSubmitting },
+  } = useForm({
     resolver: zodResolver(loginSchema),
     defaultValues: {
       email: "",
@@ -14,7 +24,86 @@ const LoginForm = () => {
     },
     mode: "all",
   });
-  return <></>;
+
+  const LoginFormHandeler = async (loginData: LoginSchematype) => {
+    console.log(loginData);
+  };
+  return (
+    <form
+      onSubmit={handleSubmit(LoginFormHandeler)}
+      className="grid gap-4"
+      noValidate>
+      <Controller
+        name="email"
+        control={control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+            <Input
+              {...field}
+              id={field.name}
+              type="email"
+              aria-invalid={fieldState.invalid}
+              placeholder="Enter your email"
+              autoComplete="email"
+            />
+            {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+          </Field>
+        )}
+      />
+
+      <Controller
+        name="password"
+        control={control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+            <Input
+              {...field}
+              id={field.name}
+              type="password"
+              aria-invalid={fieldState.invalid}
+              placeholder="Enter your password"
+              autoComplete="current-password"
+            />
+            {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+          </Field>
+        )}
+      />
+      <Controller
+        name="password"
+        control={control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <Checkbox
+              id={field.name}
+              name={field.name}
+              checked={field.value}
+              onCheckedChange={field.onChange}
+            />
+            <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+
+            {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+          </Field>
+        )}
+      />
+      <Button
+        type="submit"
+        className="cursor-pointer"
+        disabled={isSubmitting}>
+        {isSubmitting ?
+          <>
+            <LoaderIcon className="animate-spin" />
+            Submitting.....
+          </>
+        : <>
+            <FingerprintIcon />
+            Login
+          </>
+        }
+      </Button>
+    </form>
+  );
 };
 
 export default LoginForm;
